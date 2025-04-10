c42219d6804db0334454586a106af68c8776e24a
Anvid31 <mendozadiazjuandavid@gmail.com>
2025-04-10 15:04:23 -0500

    First

--- ProyectoVBack/services/socketService.js
+++ ProyectoVBack/services/socketService.js
@@ -1,23 +1,27 @@
 import { Server } from "socket.io";
 import gastosService from "./gastosService.js";
- 
 
 const setupSocket = (server) => {
-  const io = new Server(server, { cors: { origin: "http://localhost:4200" } });
+  const io = new Server(server, {
+    cors: {
+      origin: "*",
+      methods: ["GET", "POST"],
+    },
+  });
 
   io.on("connection", (socket) => {
     console.log("Usuario Conectado", socket.id);
 
     const sendGeoData = async () => {
-      const gastos = await gastosService.leerGastos()
-      const ventas = await gastosService.leerVentas()
+      const gastos = await gastosService.leerGastos();
+      const ventas = await gastosService.leerVentas();
 
       const data = {
-        gasto : gastos,
-        venta : ventas
-      }
+        gasto: gastos,
+        venta: ventas,
+      };
 
-      socket.emit('data', data);
+      socket.emit("data", data);
     };
 
     const interval = setInterval(sendGeoData, 200);
